788d5b71d47f9ee0a074abe6a3ce0f682dcc8c7c
Jean-Philippe Sirois <jp@veksen.net>
2024-08-12 10:54:21 -0400

    style: remove useless comments

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
